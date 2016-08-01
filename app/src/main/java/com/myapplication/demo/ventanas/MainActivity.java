238977dbb31aae709934b177019c3fd27f0d35ae
Anderson Eduardo Jimenez Prieto <andersonmurdog@gmail.com>
2016-08-01 12:50:17 -0500

    Terminamos de pasar datos de una ventana a  otra osea entre activitis

--- app/src/main/java/com/myapplication/demo/ventanas/MainActivity.java
+++ app/src/main/java/com/myapplication/demo/ventanas/MainActivity.java
@@ -1,21 +1,51 @@
 package com.myapplication.demo.ventanas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button boton;
+    EditText texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        Button boton;
-        EditText texto;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         boton = (Button)findViewById(R.id.button);
         texto = (EditText)findViewById(R.id.editText);
+
+        boton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.button:
+                llamar_otra_activity();
+                break;
+        }
+    }
+
+    public void llamar_otra_activity(){
+        // Creamos el intent
+        Intent intent=
+                new Intent(MainActivity.this,Segunda.class); // desde la ventana actual  (this) llamamos a la otra clase o ventana
+
+        // Creamos la informacion para pasar entre actividades
+        Bundle bundle = new Bundle();
+        // recuperamos elvalor deleditText, Para recuperar este valor en otra activity se lo ahce por mediod e la clave,en este caso NOMBRE
+        bundle.putString("NOMBRE",texto.getText().toString());
+
+        // añadimos la informacion al intent
+        intent.putExtras(bundle);
+        // iniciamos lanueva activity
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/myapplication/demo/ventanas/Segunda.java
+++ app/src/main/java/com/myapplication/demo/ventanas/Segunda.java
@@ -2,12 +2,21 @@ package com.myapplication.demo.ventanas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Segunda extends AppCompatActivity {
 
+    TextView textoSegunda;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        textoSegunda = (TextView)findViewById(R.id.textoSegunda);
+
+        // me enlazo con un contextoi actual q tengo yo mismo es this
+        Bundle b = this.getIntent().getExtras(); // con getExtras cojo las variables
+        // seteamos el valor q fue enviado de la activity main
+        textoSegunda.setText("Bienvenido "+b.getString("NOMBRE"));
     }
 }
